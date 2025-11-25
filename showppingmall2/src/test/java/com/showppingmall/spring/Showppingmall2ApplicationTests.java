3b118253cccf965fe33d5aa0c0d31369dc531192
kyutae_kim <kendm1@naver.com>
2025-11-26 01:47:41 +0900

    홈페이지 제작

---
+++ showppingmall2/src/test/java/com/showppingmall/spring/Showppingmall2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.showppingmall.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Showppingmall2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
